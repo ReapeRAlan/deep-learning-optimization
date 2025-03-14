8e73e69ec8978311f70798bf030ba6bec88a6144
ASCM5 <utm22030590@utma.edu.mx>
2025-03-13 20:04:40 -0600

    dataset_cleaned added

--- DiabeDoc.py
+++ DiabeDoc.py
@@ -320,5 +320,5 @@ if __name__ == "__main__":
         app,
         host=os.getenv("HOST", "0.0.0.0"),
         port=int(os.getenv("PORT", 8000)),
-        reload=os.getenv("RELOAD", "false").lower() == "true"
+        ##reload=os.getenv("RELOAD", "false").lower() == "true"
     )
